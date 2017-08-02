0271970c746f8adc58d3bb797d26761ccd69a0f9
Hellen S <hellensoloviy@gmail.com>
2017-08-02 18:41:02 +0300

    Change commentNode (aka DebugInProgress);

--- PinterestTexture/Structure/PinNode.swift
+++ PinterestTexture/Structure/PinNode.swift
@@ -28,6 +28,8 @@ class PinNode: ASCellNode {
         commentNode = ASTextNode()
         commentNode.maximumNumberOfLines = 3
         commentNode.attributedText = NSAttributedString(string: "Some special text for testing textNode", attributes: [NSForegroundColorAttributeName:#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)])
+        
+//        commentNode.backgroundColor = #colorLiteral(red: 0.921431005, green: 0.9214526415, blue: 0.9214410186, alpha: 1)
         self.addSubnode(commentNode)
 
     }
@@ -40,14 +42,33 @@ class PinNode: ASCellNode {
         print("Size - - w: \(size.width) h: \(size.height)")
 
         let child = ASRatioLayoutSpec(ratio: size.height/size.width, child: imageNode)
-        let finalImageLayoutSpec = ASInsetLayoutSpec(insets: .zero, child: child)
+        var insets = UIEdgeInsetsMake(0, 0, 0, 0)
+
+        let finalImageLayoutSpec = ASInsetLayoutSpec(insets: insets, child: child)
         
-        let finalTextLayoutSpec = ASInsetLayoutSpec(insets: .zero, child: commentNode)
+        insets = UIEdgeInsets(top: 0, left: 8, bottom: 4, right: 8)
+        let finalTextLayoutSpec = ASInsetLayoutSpec(insets: insets, child: commentNode)
         
-        let mainSpec = ASStackLayoutSpec(direction: .vertical, spacing: 4, justifyContent: .start, alignItems: .start, children: [finalImageLayoutSpec, finalTextLayoutSpec])
+//        let width = Int(constrainedSize.max.width)
+//        let height = (width * (object.height - 20)) / object.width
+//
+//        imageNode.style.preferredSize = CGSize(width: width, height: height)
+//        commentNode.style.preferredSize = CGSize(width: width, height: 20)
+
+        let mainSpec = ASStackLayoutSpec(direction: .vertical, spacing: 4, justifyContent: .start, alignItems: .start, children: [finalTextLayoutSpec, finalImageLayoutSpec])
+
+//        return finalImageLayoutSpec
 
         return mainSpec
+//
+//        let photoDimension: CGFloat = constrainedSize.max.width / 4.0
+//        imageNode.style.preferredSize = CGSize(width: photoDimension, height: photoDimension)
         
+        // INFINITY is used to make the inset unbounded
+//        let insets = UIEdgeInsets(top: CGFloat.infinity, left: 8, bottom: 4, right: 8)
+//        let textInsetSpec = ASInsetLayoutSpec(insets: insets, child: commentNode)
+        
+//        return ASOverlayLayoutSpec(child: finalImageLayoutSpec, overlay: textInsetSpec)
     }
 
 }
