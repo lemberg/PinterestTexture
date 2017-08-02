59b2db9cb7f9df889369adce6a73b4024d3230d2
Hellen S <hellensoloviy@gmail.com>
2017-08-02 11:29:40 +0300

    Add files;

---
+++ PinterestTexture/PinterestTexture-Header.h
@@ -0,0 +1,16 @@
+//
+//  TextureTest-Header.h
+//  TextureTest
+//
+//  Created by Hellen Soloviy on 7/31/17.
+//  Copyright © 2017 Lemberg Solutions Limited. All rights reserved.
+//
+
+#ifndef TextureTest_Header_h
+#define TextureTest_Header_h
+
+
+#import <AsyncDisplayKit/AsyncDisplayKit.h>
+
+
+#endif /* TextureTest_Header_h */
---
+++ PinterestTexture/Structure/ViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  TextureDemo
+//
+//  Created by Hellen Soloviy on 7/31/17.
+//  Copyright © 2017 Lemberg Solutions Limited. All rights reserved.
+//
+
+import UIKit
+import PinterestSDK
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func authPinterest(_ sender: UIButton!) {
+        
+        print("Let's start it!")
+        authentificate()
+        
+    }
+    
+    func authentificate() {
+        
+        PDKClient.sharedInstance().authenticate(withPermissions: [PDKClientReadPublicPermissions,PDKClientWritePublicPermissions, PDKClientReadRelationshipsPermissions,PDKClientWriteRelationshipsPermissions], from: self, withSuccess: { (responseObject) in
+            
+            PDKClient.sharedInstance().getAuthenticatedUserPins(withFields: ["id", "image", "note"], success: { (responseObject) in
+                
+                let vc = PinsViewController(with: responseObject)
+                self.navigationController?.pushViewController(vc, animated: true)
+                
+            }, andFailure: { (error) in
+                print("Error || - \(error?.localizedDescription ?? "No description provided")")
+            })
+            
+            
+        }) { (error) in
+            print("Error - \(error?.localizedDescription ?? "No description provided")")
+            
+        }
+        
+    }
+    
+}
+
